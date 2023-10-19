2a5473ef4c1f692546bf1485f1384b9d3e03a751
DwhizZ <136221482+DwhizZ@users.noreply.github.com>
2023-10-19 10:14:21 +0100

    made changes

--- lib/explore_list.dart
+++ lib/explore_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mini_mart_project/third_page.dart';
 
 
 class ExploreList extends StatelessWidget {
@@ -18,43 +19,54 @@ class ExploreList extends StatelessWidget {
           Radius.circular(10),
         ),
       ),
-      child: Stack(
-        
-        children: [
-          Container(
-            width: double.infinity,
-            height: 160,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(10),
-                topRight: Radius.circular(10),
+      child: InkWell(
+        onTap: () {
+          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return const ThirdPage();
+                              },
+                            ),
+                          );
+        },
+        child: Stack(
+          
+          children: [
+            Container(
+              width: double.infinity,
+              height: 160,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(10),
+                  topRight: Radius.circular(10),
+                ),
+                color: Color.fromARGB(255, 10, 143, 209),
               ),
-              color: Color.fromARGB(255, 10, 143, 209),
+              alignment: Alignment.center,
+              child: const Icon(
+                Icons.photo_library_outlined,
+                color: Colors.white,
+                size: 70,
+              ),
             ),
-            alignment: Alignment.center,
-            child: const Icon(
-              Icons.photo_library_outlined,
-              color: Colors.white,
-              size: 70,
+            const Align(
+              alignment: AlignmentDirectional(0, 0.3),
+              child: Text(
+                'Image Here',
+                style: TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-          const Align(
-            alignment: AlignmentDirectional(0, 0.3),
-            child: Text(
-              'Image Here',
-              style: TextStyle(
-                  color: Colors.white, fontWeight: FontWeight.bold),
-            ),
-          ),
-           Positioned(
-            bottom: 10,
-            left: 10,
-            child:  Text(
-              text,
-              style: const TextStyle(fontSize: 18, fontFamily: 'Roboto',fontWeight: FontWeight.w600),
-            ),
-          )
-        ],
+             Positioned(
+              bottom: 10,
+              left: 10,
+              child:  Text(
+                text,
+                style: const TextStyle(fontSize: 18, fontFamily: 'Roboto',fontWeight: FontWeight.w600),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
